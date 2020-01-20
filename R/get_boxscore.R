6a13832f9a7adc955bf117c8eed36c1258dfd337
lbenz730 <luke.benz@yale.edu>
2020-01-20 11:29:10 -0500

    Final Cleanup of get_boxscore()

--- R/get_boxscore.R
+++ R/get_boxscore.R
@@ -27,15 +27,17 @@ get_boxscore <- function(game_id) {
 	away <- tidyr::separate(away, '3PT', c("3PTM", "3PTA"), sep = "-")
 	away <- tidyr::separate(away, 'FT', c("FTM", "FTA"), sep = "-")
 	away_totals <- away[nrow(away):nrow(away),]
-	away_totals$Position <- ""
+	away_totals$Position <- NA
 	away <- head(away, -1)
 	away$Position <- substr(away$Starters, nchar(away$Starters), nchar(away$Starters))
 	away$Starters <- substr(away$Starters, 0, (nchar(away$Starters)-1)/2)
 	away <- rbind(away, away_totals)
 	rownames(away) <- NULL
-	colnames(away)[1] <- "Players"
+	colnames(away)[1] <- "player"
+	colnames(away)[18] <- "position"
 	away <- away[, c(1, 18, 2:(ncol(away)-1))]
-
+	away$starter <- F
+	away$starter[1:5] <- T
 
 	home <- rvest::html_table(webpage)[[3]]
 	home <- home[1:(nrow(home) - 1),]
@@ -44,22 +46,23 @@ get_boxscore <- function(game_id) {
 	home <- tidyr::separate(home, '3PT', c("3PTM", "3PTA"), sep = "-")
 	home <- tidyr::separate(home, 'FT', c("FTM", "FTA"), sep = "-")
 	home_totals <- home[nrow(home):nrow(home),]
-	home_totals$Position <- ""
+	home_totals$Position <- NA
 	home <- head(home, -1)
 	home$Position <- substr(home$Starters, nchar(home$Starters), nchar(home$Starters))
 	home$Starters <- substr(home$Starters, 0, (nchar(home$Starters)-1)/2)
 	home <- rbind(home, home_totals)
 	rownames(home) <- NULL
-	colnames(home)[1] <- "Players"
+	colnames(home)[1] <- "player"
+	colnames(home)[18] <- "position"
 	home <- home[, c(1, 18, 2:(ncol(home)-1))]
+	home$starter <- F
+	home$starter[1:5] <- T
 
 	for(i in 3:18) {
 	  home[,i] <- as.numeric(home[,i])
 	  away[,i] <- as.numeric(away[,i])
 	}
 
-	home$Position[home$Players == "TEAM"] <- NA
-	away$Position[away$Players == "TEAM"] <- NA
 
 	results <- list(away, home)
 	names(results) <- c(away_name, home_name)
