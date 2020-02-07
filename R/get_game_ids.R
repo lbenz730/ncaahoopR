0d3df7c9dc95b0b1d06f76a749833a90b7b822fa
lbenz730 <luke.benz@yale.edu>
2020-02-06 23:34:19 -0500

    HTML Fix

--- R/get_game_ids.R
+++ R/get_game_ids.R
@@ -38,7 +38,7 @@ get_game_ids <- function(team, season = current_season) {
     base_url <- "https://www.espn.com/mens-college-basketball/team/schedule/_/id/"
     url <- paste0(base_url, ids$id[ids$team == team], "/season/", as.numeric(substring(season, 1, 4)) + 1)
     x <- scan(url, what = "", sep = "\n", quiet = T)
-    y <- unlist(strsplit(x[65], "href=\"http://www.espn.com/mens-college-basketball/game\\?gameId="))
+    y <- unlist(strsplit(x[66], "href=\"http://www.espn.com/mens-college-basketball/game\\?gameId="))
     game_ids <- gsub("\".*", "", y)[-1]
 
     ### Postseason Games First
