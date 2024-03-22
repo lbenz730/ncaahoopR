bf7e01566edab477948584c6e1b3be9658305051
lbenz730 <lukesbenz@gmail.com>
2024-03-22 01:20:17 -0400

    update

--- R/wp_chart_new.R
+++ R/wp_chart_new.R
@@ -32,28 +32,31 @@ wp_chart_new <- function(game_id, home_col = NULL, away_col = NULL, include_spre
   away_team <- data$away[1]
   
   if(is.null(home_col)) {
-    home_col <- ncaa_colors$primary_color[ ncaa_colors$espn_name == dict$ESPN[dict$NCAA == home_team | 
-                                                                                dict$ESPN == home_team |
-                                                                                dict$ESPN_PBP == home_team  | 
-                                                                                dict$ESPN_PBP == gsub('State', 'St', home_team) |
-                                                                                dict$ESPN_PBP == gsub('St', 'State', home_team)][1] ]
+    home_col <- ncaa_colors$primary_color[ gsub('State', 'St', ncaa_colors$espn_name) == gsub('State', 'St', dict$ESPN[dict$NCAA == home_team | 
+                                                                                                                         dict$ESPN == home_team |
+                                                                                                                         dict$ESPN_PBP == home_team  | 
+                                                                                                                         dict$ESPN_PBP == gsub('State', 'St', home_team) |
+                                                                                                                         dict$ESPN_PBP == gsub('St', 'State', home_team)][1]) ]
   }
   if(is.null(away_col)) {
-    away_col <- ncaa_colors$primary_color[ ncaa_colors$espn_name == dict$ESPN[dict$NCAA == away_team |
-                                                                                dict$ESPN == away_team |
-                                                                                dict$ESPN_PBP == away_team | 
-                                                                                dict$ESPN_PBP == gsub('State', 'St', away_team) | 
-                                                                                dict$ESPN_PBP == gsub('St', 'State', away_team)][1] ] 
+    away_col <- ncaa_colors$primary_color[ gsub('State', 'St', ncaa_colors$espn_name) == gsub('State', 'St', dict$ESPN[dict$NCAA == away_team |
+                                                                                                                         dict$ESPN == away_team |
+                                                                                                                         dict$ESPN_PBP == away_team | 
+                                                                                                                         dict$ESPN_PBP == gsub('State', 'St', away_team) | 
+                                                                                                                         dict$ESPN_PBP == gsub('St', 'State', away_team)][1]) ] 
   }
   
-  home_url <- ncaa_colors$logo_url[ ncaa_colors$espn_name == dict$ESPN[ ncaa_colors$espn_name == dict$ESPN[dict$NCAA == home_team | 
-                                                                                                             dict$ESPN == home_team |
-                                                                                                             dict$ESPN_PBP == home_team  | 
-                                                                                                             dict$ESPN_PBP == gsub('State', 'St', home_team)][1] ]]
-  away_url <- ncaa_colors$logo_url[ ncaa_colors$espn_name == dict$ESPN[ ncaa_colors$espn_name == dict$ESPN[dict$NCAA == away_team |
-                                                                                                             dict$ESPN == away_team |
-                                                                                                             dict$ESPN_PBP == away_team | 
-                                                                                                             dict$ESPN_PBP == gsub('State', 'St', away_team)][1] ]] 
+  home_url <- ncaa_colors$logo_url[ gsub('State', 'St', ncaa_colors$espn_name) == gsub('State', 'St', dict$ESPN[dict$NCAA == home_team | 
+                                                                                                                  dict$ESPN == home_team |
+                                                                                                                  dict$ESPN_PBP == home_team  | 
+                                                                                                                  dict$ESPN_PBP == gsub('State', 'St', home_team) |
+                                                                                                                  dict$ESPN_PBP == gsub('St', 'State', home_team)][1]) ]
+  away_url <- ncaa_colors$logo_url[ gsub('State', 'St', ncaa_colors$espn_name) == gsub('State', 'St', dict$ESPN[dict$NCAA == away_team |
+                                                                                                                  dict$ESPN == away_team |
+                                                                                                                  dict$ESPN_PBP == away_team | 
+                                                                                                                  dict$ESPN_PBP == gsub('State', 'St', away_team) | 
+                                                                                                                  dict$ESPN_PBP == gsub('St', 'State', away_team)][1]) ] 
+  
   
   
   plot_lines <- 1200
